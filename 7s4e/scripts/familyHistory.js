871ac3fa004c834b0a8b4718203e99b4b5899436
Bryant <smi20149@byui.edu>
2023-06-18 14:07:20 +0800

    initialize data structures

---
+++ 7s4e/scripts/familyHistory.js
@@ -0,0 +1,53 @@
+// Family History data structures
+const branches = {}
+const trunk = {}
+const roots = {
+    april: {
+        country: "United States",
+        child: "bryant",
+        generation: 1,
+        name: "April Eyring",
+        parent: "mother",
+        year: 1956
+    },
+    bryant: {
+        country: "United States",
+        child: "",
+        generation: 0,
+        name: "Bryant Smith",
+        parent: "father",
+        year: 1975
+    } ,
+    daniel: {
+        country: "United States",
+        child: "bryant",
+        generation: 1,
+        name: "Daniel Smith",
+        parent: "father",
+        year: 1955
+    },
+    ernesto: {
+        country: "Colombia",
+        child: "vanessa",
+        generation: 1,
+        name: "Ernesto Bruno",
+        parent: "father",
+        year: 1946
+    },
+    lucia: {
+        country: "Colombia",
+        child: "vanessa",
+        generation: 1,
+        name: "Lucia Mulett",
+        parent: "mother",
+        year: 1949
+    },
+    vanessa: {
+        country: "Colombia",
+        child: "",
+        generation: 0,
+        name: "Vanessa Bruno",
+        parent: "mother",
+        year: 1974
+    }
+}
